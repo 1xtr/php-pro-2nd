b4a39adec07a4664cb04a89229374b6f4bb7b38f
1XTR.homepc <1XTR.homepc@trendco.space>
2020-11-12 11:30:12 +0300

    less2 Done

--- models/Orders.php
+++ models/Orders.php
@@ -6,6 +6,11 @@ namespace models;
 
 class Orders extends Model
 {
+    protected $id;
+    protected $user_id;
+    protected $date;
+    protected $status_id;
+    protected $totalAmount;
 
     public function getTableName(): string
     {
